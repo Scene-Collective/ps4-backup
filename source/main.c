3faf8a637e5851cf42a69fe7249a58e240f15590
Al-Azif <alazif@miskatonic.io>
2021-01-03 03:06:29 -0800

    Always do internal backup

--- source/main.c
+++ source/main.c
@@ -1,5 +1,19 @@
 #include "ps4.h"
 
+int internal_backup() {
+  unlink("/system_data/priv/mms/app.bak");
+  unlink("/system_data/priv/mms/addcont.bak");
+  unlink("/system_data/priv/mms/av_content_bg.bak");
+  unlink("/user/system/webkit/secure/appcache/ApplicationCache.bak");
+  unlink("/user/system/webkit/webbrowser/appcache/ApplicationCache.bak");
+  copy_file("/system_data/priv/mms/app.db", "/system_data/priv/mms/app.bak");
+  copy_file("/system_data/priv/mms/addcont.db", "/system_data/priv/mms/addcont.bak");
+  copy_file("/system_data/priv/mms/av_content_bg.db", "/system_data/priv/mms/av_content_bg.bak");
+  copy_file("/user/system/webkit/secure/appcache/ApplicationCache.db", "/user/system/webkit/secure/appcache/ApplicationCache.bak");
+  copy_file("/user/system/webkit/webbrowser/appcache/ApplicationCache.db", "/user/system/webkit/webbrowser/appcache/ApplicationCache.bak");
+  return 0;
+}
+
 int _main(struct thread *td) {
   UNUSED(td);
 
@@ -14,15 +28,12 @@ int _main(struct thread *td) {
   if (!file_exists("/mnt/usb0/.probe")) {
     touch_file("/mnt/usb1/.probe");
     if (!file_exists("/mnt/usb1/.probe")) {
-      copy_file("/system_data/priv/mms/app.db", "/system_data/priv/mms/app.bak");
-      copy_file("/system_data/priv/mms/addcont.db", "/system_data/priv/mms/addcont.bak");
-      copy_file("/system_data/priv/mms/av_content_bg.db", "/system_data/priv/mms/av_content_bg.bak");
-      copy_file("/user/system/webkit/secure/appcache/ApplicationCache.db", "/user/system/webkit/secure/appcache/ApplicationCache.bak");
-      copy_file("/user/system/webkit/webbrowser/appcache/ApplicationCache.db", "/user/system/webkit/webbrowser/appcache/ApplicationCache.bak");
+      internal_backup();
       printf_notification("Internal backup complete.\nThis was only a database backup, use a USB device for a full backup.");
     } else {
       printf_notification("Backing up to USB1");
       unlink("/mnt/usb1/.probe");
+      internal_backup();
       mkdir("/mnt/usb1/PS4/", 0777);
       rmtree("/mnt/usb1/PS4/Backup/");
       mkdir("/mnt/usb1/PS4/Backup/", 0777);
@@ -61,6 +72,7 @@ int _main(struct thread *td) {
   } else {
     printf_notification("Backing up to USB0");
     unlink("/mnt/usb0/.probe");
+    internal_backup();
     mkdir("/mnt/usb0/PS4/", 0777);
     rmtree("/mnt/usb0/PS4/Backup/");
     mkdir("/mnt/usb0/PS4/Backup/", 0777);
